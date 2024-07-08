7fea0ae60606d12b08917a21e73d3a0881dc9873
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-07-09 02:31:09 +0530

    Time: 2 ms (75.49%), Space: 7.1 MB (77.09%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int> v;
+        
+        int ans = 0;
 
-        for(int i = 0; i < n; i++)
-        v.push_back(i+1);
+        for(int i = 2; i <= n; i++)
+        ans = (ans + k) % i;
 
-        int index = 0;
-
-        while(v.size() > 1){
-            index = (index + (k - 1)) % v.size();
-            v.erase(v.begin() + index);
-        }
-
-        return v[0];
+        return (ans + 1);
     }
 };
