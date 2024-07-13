1afee546879d3424686c9ee27874cae86c009c3c
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-07-14 03:16:57 +0530

    Time: 493 ms (12.96%), Space: 305.6 MB (7.78%) - LeetHub

--- 2846-robot-collisions/2846-robot-collisions.cpp
+++ 2846-robot-collisions/2846-robot-collisions.cpp
@@ -1,57 +1,68 @@
 class Solution {
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        int n=positions.size();
-        vector<pair<int,vector<int>>>v(n);
-        for(int i=0;i<n;i++){
-            v[i]={positions[i],{i,healths[i],directions[i]}};
-        }
-        sort(v.begin(),v.end());
-        stack<pair<int,vector<int>>>st;
-        vector<pair<int,vector<int>>>ans;
-        for(int i=0;i<n;i++){
-            if(v[i].second[2]=='R'){
-                st.push(v[i]);
-            }
+        int n = positions.size();
+        vector<pair<int, vector<int>>> v(n);
+
+        for(int i = 0; i < n; i++)
+        v[i] = {positions[i],{i, healths[i], directions[i]}};
+
+        sort(v.begin(), v.end());
+
+        stack<pair<int, vector<int>>> st;
+        vector<pair<int, vector<int>>> ans;
+
+        for(int i = 0; i < n; i++){
+            if(v[i].second[2] == 'R')
+            st.push(v[i]);
+
             else{
-                bool ok=true;
+                bool ok = true; // assume that upcoming robot with L will win.
+
                 while(!st.empty()){
-                    pair<int,vector<int>> tp=st.top();
+                    pair<int, vector<int>> tp = st.top();
                     st.pop();
-                    if(tp.second[1]==v[i].second[1]){
-                        ok=false;
+
+                    if(tp.second[1] == v[i].second[1]){
+                        ok = false;
                         break;
                     }
-                    else if(tp.second[1]>v[i].second[1]){
-                        ok=false;
+
+                    else if(tp.second[1] > v[i].second[1]){
                         tp.second[1]--;
+                        ok = false;
                         st.push(tp);
                         break;
                     }
+
                     else{
                         v[i].second[1]--;
-                        continue;
                     }
                 }
-                if(ok){
-                    ans.push_back(v[i]);
-                }
+
+                if(ok)
+                ans.push_back(v[i]);
             }
         }
+
         while(!st.empty()){
-            ans.push_back(st.top());
+            ans.push_back(st.top()); // In case only R's are there.
             st.pop();
         }
-        for(int i=0;i<ans.size();i++){
-            pair<int,vector<int>>temp=ans[i];
-            ans[i].first=temp.second[0];
-            ans[i].second[0]=temp.first;
+        
+        for(int i = 0; i < ans.size(); i++){
+            swap(ans[i].first, ans[i].second[0]); // in order to sort it acc. to index now.
         }
-        sort(ans.begin(),ans.end());
-        vector<int>res;
-        for(int i=0;i<ans.size();i++){
-            res.push_back(ans[i].second[1]);
-        }
+
+        sort(ans.begin(), ans.end());
+
+        vector<int> res;
+
+        for(int i = 0; i < ans.size(); i++)
+        res.push_back(ans[i].second[1]);
+
         return res;
     }
+
+    
 };
