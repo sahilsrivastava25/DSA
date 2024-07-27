4522d174e86dc57aacf002bb3d21845f7d4f6fa8
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-07-27 12:43:58 +0530

    Time: 0 ms (100%), Space: 9.1 MB (21.43%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -14,7 +14,7 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast != NULL && fast->next != NULL){
+        while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
         }
