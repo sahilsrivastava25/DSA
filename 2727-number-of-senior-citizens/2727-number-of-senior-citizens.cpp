f10414a5ef198887851bcab2caf7b82f40e94127
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-08-02 00:04:40 +0530

    Time: 12 ms (13.14%), Space: 17.7 MB (16.71%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -2,17 +2,10 @@ class Solution {
 public:
     int countSeniors(vector<string>& details) {
         int ans = 0;
-        
-        for(int i = 0; i < details.size(); i++){
-            int age1 = details[i][11] - '0';
-            int age2 = details[i][12] - '0';
-            if(age1 > 6)
-            ans++;
-
-            else if(age1 == 6 && (age2 > 0 && age2 <= 9))
-            ans++;
+        for (auto& x : details) {
+            int age = stoi(x.substr(11, 2));
+            ans += age > 60;
         }
-
         return ans;
     }
 };
