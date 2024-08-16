c74733d0d586dd778a2c705e710faf8381f303e8
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-08-17 01:07:57 +0530

    Time: 71 ms (60.53%), Space: 85.8 MB (81.87%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int fivebillcount=0,tenbillcount=0;
-        for(int i=0;i<bills.size();i++){
-            if(bills[i]==5) fivebillcount++;
-            else if(bills[i]==10){
-                if(fivebillcount>=1){
-                    tenbillcount++;
-                    fivebillcount--;
+        int five_dollars = 0, ten_dollars = 0;
+        
+        for (int x : bills) {
+            if (x == 5) {
+                five_dollars++;
+            } else if (x == 10) {
+                if (five_dollars > 0) {
+                    five_dollars--;
+                    ten_dollars++;
+                } else {
+                    return false;
                 }
-                else return false;
-            }
-            else{
-                if(tenbillcount>=1 && fivebillcount>=1){
-                    tenbillcount--;
-                    fivebillcount--;
+            } else {
+                if (five_dollars > 0 && ten_dollars > 0) {
+                    five_dollars--;
+                    ten_dollars--;
+                } else if (five_dollars > 2) {
+                    five_dollars -= 3;
+                } else {
+                    return false;
                 }
-                else if(fivebillcount>=3){
-                    fivebillcount-=3;
-                }
-                else return false;
             }
         }
+        
         return true;
     }
 };
