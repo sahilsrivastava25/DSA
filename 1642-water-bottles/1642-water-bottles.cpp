3b0b1c09140bbfb408c5e3170121c3ff58f78079
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-08-19 01:57:06 +0530

    Time: 2 ms (53.88%), Space: 7.6 MB (7.1%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    int numWaterBottles(int numBottles, int numExchange) {
-        int y = numBottles;
-        int x = 0; //empty bottles
-        int ans = 0;
-        while(y){
-            ans = ans + y;
-            x = x + y;
-            y = x / numExchange;
-            x = x % numExchange;
-        }
-
-        return ans;
+    static int exchange(int n, int k){
+        if (n<k) return 0;
+        auto [n0, r]=div(n, k);
+        return n0+exchange(n0+r, k);
+    }
+    static int numWaterBottles(int n, int k) {
+        return n+exchange(n, k);
     }
 };
+
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
