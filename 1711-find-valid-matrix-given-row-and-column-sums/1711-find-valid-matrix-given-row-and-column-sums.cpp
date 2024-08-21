67a41e7108fec8ec3bc91c11e44b48c2f15f8133
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-08-21 16:24:38 +0530

    Time: 28 ms (88.64%), Space: 36.2 MB (46.28%) - LeetHub

--- 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,35 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
-        int rows = rowSum.size();
-        int cols = colSum.size();
-        int cur_row = 0, cur_col = 0;
-        vector<vector<int>> res(rows, vector<int>(cols, 0));
+    std::vector<std::vector<int>> restoreMatrix(std::vector<int>& rowSum, std::vector<int>& colSum) {
+        int m = rowSum.size();
+        int n = colSum.size();
+        std::vector<std::vector<int>> matrix(m, std::vector<int>(n, 0));
+        int i = m - 1, j = n - 1;
 
-        while (cur_row < rows || cur_col < cols) {
-            if (cur_row >= rows) {
-                res[rows - 1][cur_col] = colSum[cur_col];
-                cur_col++;
-                continue;
-            } else if (cur_col >= cols) {
-                res[cur_row][cols - 1] = rowSum[cur_row];
-                cur_row++;
-                continue;
-            }
-
-            int value_to_put = min(rowSum[cur_row], colSum[cur_col]);
-            rowSum[cur_row] -= value_to_put;
-            colSum[cur_col] -= value_to_put;
-            res[cur_row][cur_col] = value_to_put;
-
-            // I write this as this because it's possible that rowSum[cur_row] == colSum[cur_col] and we'll want to move both row and col pointers
-            if (rowSum[cur_row] == 0) {
-                cur_row++;
-            }
-            if (colSum[cur_col] == 0) {
-                cur_col++;
+        while (i >= 0 && j >= 0) {
+            if (rowSum[i] <= colSum[j]) {
+                matrix[i][j] = rowSum[i];
+                colSum[j] -= rowSum[i];
+                i--;
+            } else {
+                matrix[i][j] = colSum[j];
+                rowSum[i] -= colSum[j];
+                j--;
             }
         }
-        return res;
+
+        return matrix;
     }
 };
