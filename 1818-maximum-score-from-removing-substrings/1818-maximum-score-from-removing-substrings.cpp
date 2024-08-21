8f17b50ceb43466e1f5120c7785370f7c68c8ecc
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-08-21 16:15:43 +0530

    Time: 77 ms (57.84%), Space: 26.8 MB (35.11%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,67 +1,44 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        int max = 0;
+        int res = 0;
+        string top, bot;
+        int top_score, bot_score;
 
-        if(y > x){
-            for(int i = 0; i < s.size(); i++){
-              if(i+1 < s.size() && s[i] == 'b' && s[i+1]=='a'){
-                max += y;
-                s.erase(i, 2);
-
-                if(i > 0)
-                i = i - 2;
-
-                else
-                i--;
-              }
-            }
-
-            for(int i = 0; i < s.size(); i++){
-              if(i+1 < s.size() && s[i] == 'a' && s[i+1]=='b'){
-                max += x;  
-                s.erase(i, 2);
-                
-                if(i > 0)
-                i = i - 2;
-
-                else
-                i--;
-              }   
-            }
-
+        if (y > x) {
+            top = "ba";
+            top_score = y;
+            bot = "ab";
+            bot_score = x;
+        } else {
+            top = "ab";
+            top_score = x;
+            bot = "ba";
+            bot_score = y;
         }
 
-        else{
-            for(int i = 0; i < s.size(); i++){
-              if(i+1 < s.size() && s[i] == 'a' && s[i+1]=='b'){
-                max += x;
-                s.erase(i, 2);
-                
-                if(i > 0)
-                i = i - 2;
-
-                else
-                i--;
-              }
+        // Removing first top substrings cause they give more points
+        vector<char> stack;
+        for (char ch : s) {  // Changed 'char' to 'ch'
+            if (ch == top[1] && !stack.empty() && stack.back() == top[0]) {
+                res += top_score;
+                stack.pop_back();
+            } else {
+                stack.push_back(ch);
             }
-
-            for(int i = 0; i < s.size(); i++){
-              if(i+1 < s.size() && s[i] == 'b' && s[i+1]=='a'){
-                max += y;  
-                s.erase(i, 2);
-                
-                if(i > 0)
-                i = i - 2;
-
-                else
-                i--;
-              }   
-            }
-
         }
-        return max;
 
+        // Removing bot substrings cause they give less or equal amount of scores
+        vector<char> new_stack;
+        for (char ch : stack) {  // Changed 'char' to 'ch'
+            if (ch == bot[1] && !new_stack.empty() && new_stack.back() == bot[0]) {
+                res += bot_score;
+                new_stack.pop_back();
+            } else {
+                new_stack.push_back(ch);
+            }
+        }
 
+        return res;
     }
 };
