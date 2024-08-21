e8ae0f92233a16e22ceacc98634ac892c9ace787
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-08-21 16:16:11 +0530

    Time: 135 ms (98.67%), Space: 197.3 MB (70.19%) - LeetHub

--- 2846-robot-collisions/2846-robot-collisions.cpp
+++ 2846-robot-collisions/2846-robot-collisions.cpp
@@ -1,68 +1,39 @@
 class Solution {
 public:
-    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        int n = positions.size();
-        vector<pair<int, vector<int>>> v(n);
+    using int2=pair<int, int>;// (position, idx)
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string& directions) {
+        const int n=positions.size();
+        vector<int2> robot(n);
+        for(int i=0; i<n; i++)// 0-indexed is fine
+            robot[i]={positions[i], i};
+        sort(robot.begin(), robot.end(), greater<>());
 
-        for(int i = 0; i < n; i++)
-        v[i] = {positions[i],{i, healths[i], directions[i]}};
-
-        sort(v.begin(), v.end());
-
-        stack<pair<int, vector<int>>> st;
-        vector<pair<int, vector<int>>> ans;
-
-        for(int i = 0; i < n; i++){
-            if(v[i].second[2] == 'R')
-            st.push(v[i]);
-
+        vector<int> stack;
+        for(auto& [pos, i]: robot){
+            if (directions[i]=='L') stack.push_back(i);
             else{
-                bool ok = true; // assume that upcoming robot with L will win.
-
-                while(!st.empty()){
-                    pair<int, vector<int>> tp = st.top();
-                    st.pop();
-
-                    if(tp.second[1] == v[i].second[1]){
-                        ok = false;
-                        break;
-                    }
-
-                    else if(tp.second[1] > v[i].second[1]){
-                        tp.second[1]--;
-                        ok = false;
-                        st.push(tp);
-                        break;
-                    }
-
-                    else{
-                        v[i].second[1]--;
-                    }
+                while(!stack.empty() && healths[i]>0){
+                    int j=stack.back();
+                    int x=healths[j]-healths[i];
+                    if (x>0) healths[j]--, healths[i]=0;
+                    else if (x<0) healths[j]=0, healths[i]--, stack.pop_back();
+                    else healths[i]=healths[j]=0, stack.pop_back();
                 }
-
-                if(ok)
-                ans.push_back(v[i]);
             }
         }
-
-        while(!st.empty()){
-            ans.push_back(st.top()); // In case only R's are there.
-            st.pop();
-        }
-        
-        for(int i = 0; i < ans.size(); i++){
-            swap(ans[i].first, ans[i].second[0]); // in order to sort it acc. to index now.
-        }
-
-        sort(ans.begin(), ans.end());
-
-        vector<int> res;
-
-        for(int i = 0; i < ans.size(); i++)
-        res.push_back(ans[i].second[1]);
-
-        return res;
+        vector<int> ans;
+        for(int x: healths)
+            if (x>0) ans.push_back(x);
+        return ans;
     }
-
-    
 };
+
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
