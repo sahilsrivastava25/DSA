71c8b2e8b207af6961c5b0c8cc9b8db210cfa04c
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-08-21 16:17:07 +0530

    Time: 2 ms (58.78%), Space: 9.2 MB (84.79%) - LeetHub

--- 0726-number-of-atoms/0726-number-of-atoms.cpp
+++ 0726-number-of-atoms/0726-number-of-atoms.cpp
@@ -1,57 +1,97 @@
 class Solution {
 public:
-    string countOfAtoms(string formula) {
-        stack<unordered_map<string, int>> stk;
-        stk.push({}); // Initialize with an empty map
-        
-        int i = 0, n = formula.length();
-        
-        while (i < n) {
+    using int3 = array<int, 3>; // (pos for right ')', real ending, multiplier)
+    vector<int3> mult;
+    int n;
+
+    unordered_map<string, int> atom; 
+
+    // Recursive function to count atoms TC: O(n)
+    void f(int i, string& formula, int r, int times = 1) {
+        while (i < r) {
             if (formula[i] == '(') {
-                stk.push({});
-                i++;
-            } else if (formula[i] == ')') {
-                unordered_map<string, int> top = stk.top();
-                stk.pop();
-                i++;
+                auto& m = mult[i];
+                f(i+1, formula, m[0], m[2] * times);
+                i = m[1]+1;
+            } 
+            else if (isupper(formula[i])) {
+                string chemi = string(1, formula[i]);
                 int start = i;
-                while (i < n && isdigit(formula[i])) {
+                while (i+1 < r && islower(formula[i+1])) {
+                    chemi+= formula[i+1];
                     i++;
                 }
-                int multiplier = start < i ? stoi(formula.substr(start, i - start)) : 1;
-                for (auto& kv : top) {
-                    stk.top()[kv.first] += kv.second * multiplier;
-                }
-            } else {
-                int start = i++;
-                while (i < n && islower(formula[i])) {
+                i++;
+
+                int x = 0;
+                while (i < r && isdigit(formula[i])) {
+                    x=x*10 + (formula[i]-'0');
                     i++;
                 }
-                string element = formula.substr(start, i - start);
-                start = i;
-                while (i < n && isdigit(formula[i])) {
-                    i++;
-                }
-                int count = start < i ? stoi(formula.substr(start, i - start)) : 1;
-                stk.top()[element] += count;
-            }
+                if (x== 0) x= 1;
+                atom[chemi]+= x* times;
+            } 
+            else 
+                i++;
         }
-        
-        unordered_map<string, int> result = stk.top();
-        vector<string> elements;
-        for (auto& kv : result) {
-            elements.push_back(kv.first);
-        }
-        sort(elements.begin(), elements.end());
-        
-        string res;
-        for (const auto& element : elements) {
-            res += element;
-            if (result[element] > 1) {
-                res += to_string(result[element]);
-            }
-        }
-        
-        return res;
     }
+
+    // Print the content of the container
+    void print(auto& c) {
+        int sz=c.size();
+        for (int i=0; i<n; i++){
+            auto& x=c[i];
+            if (x[0]!=-1)
+                cout<<i<<" ->[" << x[0] << "," << x[1] << "," << x[2]<<"]\n";
+        }
+    }
+
+    // Convert atom to result string TC: O(klog k)
+    inline string convert() {
+        vector<pair<string, int>> satom(atom.begin(),atom.end());
+        sort(satom.begin(), satom.end());
+        string result;
+        for (auto& [chemi, x]: satom) {
+            result += chemi;
+            if (x > 1)
+                result += to_string(x);
+            
+        }
+        return result;
+    }
+
+    string countOfAtoms(string& formula) {
+        n=formula.size();
+        atom.reserve(n);
+        vector<int> st;
+        mult.assign(n, {-1, -1, -1});
+        for (int i = 0; i < n; i++) {// O(n)
+            char c = formula[i];
+            if (c == '(')
+                st.push_back(i);
+            else if (c == ')') {
+                int x = 0, r = i;
+                while (i + 1 < n && isdigit(formula[i + 1])) {
+                    x = 10 * x + (formula[++i] - '0');
+                }
+                if (x == 0)
+                    x = 1;
+                int l = st.back();
+                st.pop_back();
+                mult[l] = {r, i, x};
+            }
+        }
+    //    print(mult);
+        
+        f(0, formula, n, 1); // Start recursive function
+        return convert();       // Convert the atom array to result string
+    }
+
 };
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
