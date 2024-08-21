e59a8332f821762ac00c00bcc968329d1673ce2d
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-08-21 16:20:59 +0530

    Time: 16 ms (59.69%), Space: 27.4 MB (34.65%) - LeetHub

--- 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.cpp
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.cpp
@@ -1,37 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    vector<TreeNode*>res;
-    unordered_set<int>st;
-    void deleteNode(TreeNode* &root){
-        if(root){
-            deleteNode(root->left);
-            deleteNode(root->right);
-            if(st.find(root->val)!=st.end()){
-                if(root->left) res.push_back(root->left);
-                if(root->right) res.push_back(root->right);
-                root=NULL;
-                
+public:
+    const static int N = 1001;
+    bitset<N> rmv = 0;
+    vector<TreeNode*> ans;
+
+    void dfs(TreeNode* root, TreeNode* parent, bool isLeft) {
+        if (!root) return;
+        if (rmv[root->val]) {
+            if (parent) {
+                if (isLeft) parent->left = NULL;
+                else parent->right = NULL;
             }
+            dfs(root->left, NULL, 1);
+            dfs(root->right, NULL, 0);
+            delete root;  // Free the memory of the deleted node
+        } 
+        else {
+            if (!parent) 
+                ans.push_back(root);
+
+            dfs(root->left, root, 1);
+            dfs(root->right, root, 0);
         }
     }
 
-public:
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-        for(int i=0;i<to_delete.size();i++){
-            st.insert(to_delete[i]);
-        }
-        deleteNode(root);
-        if(root)res.push_back(root);
-        return res;
+        for (int x : to_delete) 
+            rmv[x] = 1;
+        dfs(root, NULL, 1);
+        return ans;
     }
 };
