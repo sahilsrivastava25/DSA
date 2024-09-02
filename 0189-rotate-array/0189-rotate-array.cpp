c6c3c4d03581b94914645b29baa1aa38170c3d26
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-09-03 02:59:08 +0530

    Time: 16 ms (86.08%), Space: 27.4 MB (86.62%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,12 +3,10 @@ public:
     void rotate(vector<int>& nums, int k) {
         ios::sync_with_stdio(false);
         cin.tie(NULL);
-
         int n = nums.size();
-        int ro = k % n;
-
+        k = k % n;
         reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + ro);
-        reverse(nums.begin() + ro, nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
