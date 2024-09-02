b189ef6ffe0aead796d28f4346c2a509fcc49a99
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-09-02 20:57:39 +0530

    Time: 101 ms (35.03%), Space: 83.9 MB (5.65%) - LeetHub

---
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+       long long int n = chalk.size(), sum = 0;
+        vector<long long int> v(n);
+
+        v[0] = chalk[0];
+        for(int i = 1; i < n ; i++) v[i] = v[i-1] + chalk[i];
+
+        for(int i = 0; i < n; i++) sum += chalk[i];
+
+        long long int ans = k % sum;
+        
+        int i = 0;
+        for(i = 0; i < n; i++){
+            if(ans < v[i]) break;
+        }
+
+        return i;
+    }
+};
