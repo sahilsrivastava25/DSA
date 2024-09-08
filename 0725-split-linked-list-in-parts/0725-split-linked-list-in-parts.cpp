432d9cd89c744b0e4008f2c6a027e45c7f1d5488
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-09-09 01:52:58 +0530

    Time: 4 ms (73.67%), Space: 13.8 MB (62.77%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -1,63 +1,30 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+    vector<ListNode*> splitListToParts(ListNode* head, int k) 
+    {
+        ListNode* curr=head;
+        int n=0;
+        while(curr){
+            curr=curr->next;
+            n++;
+        }
+        auto [q, r]=div(n, k);
+       
+        vector<int> iLen(k, q);
+        fill(iLen.begin(), iLen.begin()+r, q+1);//Use fill
         vector<ListNode*> ans(k);
-
-        // get total size of linked list
-        int size = 0;
-        ListNode* current = head;
-        while (current != nullptr) {
-            size++;
-            current = current->next;
+        curr=head;
+        for(int i=0; i<k; i++){
+            ans[i]=curr;
+            for (int j=0; j < iLen[i]-1; j++) {
+                curr=curr->next;
+            }
+            if (curr) { //Check if curr is not null
+                ListNode* prev=curr; //extra pointer is needed
+                curr=curr->next;
+                prev->next=NULL; //Remove the link
+            }
         }
-
-        // minimum size for the k parts
-        int splitSize = size / k;
-
-        // Remaining nodes after splitting the k parts evenly.
-        // These will be distributed to the first (size % k) nodes
-        int numRemainingParts = size % k;
-
-        current = head;
-        ListNode* prev = current;
-        for (int i = 0; i < k; i++) {
-            // create the i-th part
-            ListNode* newPart = current;
-            // calculate size of i-th part
-            int currentSize = splitSize;
-            if (numRemainingParts > 0) {
-                numRemainingParts--;
-                currentSize++;
-            }
-
-            // traverse to end of new part
-            int j = 0;
-            while (j < currentSize) {
-                prev = current;
-                if (current != nullptr) {
-                    current = current->next;
-                }
-                j++;
-            }
-
-            // cut off the rest of linked list
-            if (prev != nullptr) {
-                prev->next = nullptr;
-            }
-
-            ans[i] = newPart;
-        }
-
         return ans;
     }
 };
