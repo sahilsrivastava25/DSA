d081d29fa56430db72c4fa4b069ad0fd0e065317
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-09-11 01:58:53 +0530

    Time: 34 ms (81.91%), Space: 35.6 MB (49.49%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.cpp
@@ -1,42 +1,29 @@
- static const int __ = [](){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
-}();
-
-int init = [] {
-    ofstream out("user.out");
-    cout.rdbuf(out.rdbuf());
-    for (string str; getline(cin, str); cout << '\n') {
-        if (str.find(',') == string::npos) {
-            cout << str;
-            continue;
-        }
-        stringstream ss(str); ss.ignore();
-        int prev; ss >> prev; ss.ignore();
-        cout << '[' << prev;
-        for (int curr; ss >> curr; ss.ignore()) {
-            cout << ',' << gcd(prev, curr) << ',' << curr;
-            prev = curr;
-        }
-        cout << ']';
-    }
-    exit(0);
-    return 0;
-}();
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
-        if(head->next == NULL)return head;
-        ListNode * temp = head->next,*prev = head;
-        while(temp!=NULL){
-            int k = gcd(prev->val,temp->val);
-            ListNode * newnode = new ListNode(k);
-            newnode->next = temp;
-            prev->next = newnode;
-            prev = temp;
-            temp = temp->next;
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        if(head->next == NULL) return head;
+
+        ListNode* temp = head;
+        
+        while(temp && temp->next){
+            ListNode* midNode = new ListNode();
+            midNode->val = gcd(temp->val, temp->next->val);
+            midNode->next = temp->next;
+            temp->next = midNode;
+            temp = temp->next->next;
         }
         return head;
     }
