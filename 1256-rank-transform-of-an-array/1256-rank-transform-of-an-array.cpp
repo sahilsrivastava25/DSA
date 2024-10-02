874be4fa2186517c2027bb730c9f40297154958e
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-10-02 15:31:02 +0530

    Time: 109 ms (19.31%), Space: 43.8 MB (43.55%) - LeetHub

---
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        map<int, int> mp;
+        for(int i = 0; i < arr.size(); i++) mp[arr[i]] = 0;
+
+        int rank = 1;
+        for(auto val = mp.begin(); val != mp.end(); val++){
+            val->second = rank;
+            rank++;
+        }
+
+        vector<int> ans;
+        for(int i = 0; i < arr.size(); i++) ans.push_back(mp[arr[i]]);
+
+        return ans;
+    }
+};
