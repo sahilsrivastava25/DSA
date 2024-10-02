b9ace9c25a30bdf946a91b61bc417368376e38cf
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-10-02 15:31:52 +0530

    Time: 87 ms (42.65%), Space: 43.8 MB (43.96%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        
         map<int, int> mp;
         for(int i = 0; i < arr.size(); i++) mp[arr[i]] = 0;
 
