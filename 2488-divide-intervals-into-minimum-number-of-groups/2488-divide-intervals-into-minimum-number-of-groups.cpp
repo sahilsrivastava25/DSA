0e4c6c5d77be723ea121f54a1663044fd2382db7
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-10-12 14:00:13 +0530

    Time: 189 ms (99.55%), Space: 93.1 MB (75.9%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    int minGroups(vector<vector<int>>& intervals) {
-        vector<int> start_times, end_times;
-
-        // Extract start and end times
-        for (const auto& interval : intervals) {
-            start_times.push_back(interval[0]);
-            end_times.push_back(interval[1]);
+    static int minGroups(vector<vector<int>>& intervals) {
+        const int n=intervals.size();
+        vector<int> P;
+        P.reserve(n*2);
+        for(auto& I: intervals){
+            int x=I[0], y=I[1]+1;
+            P.push_back((x<<1)+1);
+            P.push_back(y<<1);
         }
-
-        // Sort start and end times
-        sort(start_times.begin(), start_times.end());
-        sort(end_times.begin(), end_times.end());
-
-        int end_ptr = 0, group_count = 0;
-
-        // Traverse through the start times
-        for (int start : start_times) {
-            if (start > end_times[end_ptr]) {
-                end_ptr++;
-            } else {
-                group_count++;
-            }
+        sort(P.begin(), P.end());
+        int cnt=0, x=0;
+        for(int z: P){
+            x+=(z&1)?1:-1;
+            cnt=max(cnt, x);
         }
-
-        return group_count;
+        
+        return cnt;    
     }
 };
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
