8fe084698a1d56150bab8f7dc3632941be5665ad
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-10-14 01:30:34 +0530

    Time: 10 ms (83.58%), Space: 19.6 MB (66.07%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++) ans = ans ^ nums[i];
+        return ans;
+    }
+};
