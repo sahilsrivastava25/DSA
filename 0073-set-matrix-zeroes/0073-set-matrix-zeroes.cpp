c9d06cd2736d476a85e912468824f3076fa32b9a
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-10-14 04:51:26 +0530

    Time: 7 ms (84.36%), Space: 16.9 MB (56.31%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,19 +5,22 @@ public:
         cin.tie(NULL);
         
         int col0 = 1;
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[0].size(); j++){
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 if(matrix[i][j] == 0){
                     matrix[i][0] = 0;
-
                     if(j != 0) matrix[0][j] = 0;
                     else col0 = 0;
                 }
             }
         }
 
-        for(int i = 1; i < matrix.size(); i++){
-            for(int j = 1; j < matrix[0].size(); j++){
+
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
                 if(matrix[i][j] != 0){
                     if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
                 }
@@ -25,11 +28,15 @@ public:
         }
 
         if(matrix[0][0] == 0){
-            for(int i = 0; i < matrix[0].size(); i++) matrix[0][i] = 0;
+            for(int j = 0; j < m; j++){
+                matrix[0][j] = 0;
+            }
         }
 
         if(col0 == 0){
-            for(int i = 0; i < matrix.size(); i++) matrix[i][0] = 0;
+            for(int i = 0; i < n; i++){
+                matrix[i][0] = 0;
+            }
         }
     }
 };
