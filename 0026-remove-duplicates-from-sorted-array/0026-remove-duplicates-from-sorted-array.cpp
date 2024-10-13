bacf479c2dff381f1a925e7903e6a091324a16ef
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2024-10-14 00:36:25 +0530

    Time: 4 ms (84.92%), Space: 21.4 MB (17.87%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+       ios::sync_with_stdio(false);
+       cin.tie(NULL);
+
+       int i = 0;
+       for(int j = 1; j < nums.size(); j++){
+        if(nums[i] != nums[j]){
+            nums[i+1] = nums[j];
+            i++;
+        }
+       }
+
+       return (i+1);
+    }
+};
