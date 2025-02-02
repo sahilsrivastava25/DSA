7fc0e20ab383effd8a916c404b7e32b45a0a9685
Sahil Srivastava <115632717+sahilsrivastava25@users.noreply.github.com>
2025-02-03 00:30:39 +0530

    Time: 0 ms (100%), Space: 11.6 MB (5.21%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        ios::sync_with_stdio(false);
-        cin.tie(NULL);
+        int n = nums.size();
 
-        int k;
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] < nums[i-1]){
-                k = i;
-                break;
+        // Construct the rotated array
+        vector<int> checkSorted(n);
+
+        // Iterate through all possible rotation offsets
+        for (int rotationOffset = 0; rotationOffset < n; ++rotationOffset) {
+            int currIndex = 0;
+            for (int index = rotationOffset; index < n; ++index) {
+                checkSorted[currIndex++] = (nums[index]);
+            }
+            for (int index = 0; index < rotationOffset; ++index) {
+                checkSorted[currIndex++] = (nums[index]);
+            }
+
+            // Check if the constructed array is sorted
+            bool isSorted = true;
+            for (int index = 0; index < n - 1; ++index) {
+                if (checkSorted[index] > checkSorted[index + 1]) {
+                    isSorted = false;
+                    break;
+                }
+            }
+
+            // If sorted, return true
+            if (isSorted) {
+                return true;
             }
         }
 
-        if(k == nums.size()) return true;
-
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end());
-        reverse(nums.begin(), nums.end());
-
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] < nums[i-1]) return false;
-        }
-
-        return true;
+        // If no rotation makes the array sorted, return false
+        return false;
     }
 };
